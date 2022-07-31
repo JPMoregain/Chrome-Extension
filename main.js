3f5577739a6a326ebbb1c358b668709e53dca3e5
JP Moregain <j.p.moregain15@gmail.com>
2022-07-31 12:31:17 -0400

    Add popup and image to replace blocked content

--- main.js
+++ main.js
@@ -1,2 +1,5 @@
-const contents = document.getElementById('contents');
-contents.parentNode.removeChild(contents);
+var contents = $('#contents');
+var contentsParent = contents.parent();
+contents.remove();
+contentsParent.prepend('<img src="https://thumbs.dreamstime.com/b/life-inspirational-quotes-be-stronger-than-your-excuses-blurry-background-motivational-170115740.jpg"/>');
+contentsParent.prepend('<p class="beautText">Imagine how productive you can be without watching YouTube videos!</p>');
