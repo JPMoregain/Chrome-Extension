fdaa0bbccb911c47452bfcc2c2d177ed79af39f5
JP Moregain <j.p.moregain15@gmail.com>
2022-08-02 09:17:50 -0400

    Add randomized images on load, bug fixes

--- main.js
+++ main.js
@@ -1,5 +1,18 @@
 var contents = $('#contents');
 var contentsParent = contents.parent();
 contents.remove();
-contentsParent.prepend('<img src="https://thumbs.dreamstime.com/b/life-inspirational-quotes-be-stronger-than-your-excuses-blurry-background-motivational-170115740.jpg"/>');
-contentsParent.prepend('<p class="beautText">Imagine how productive you can be without watching YouTube videos!</p>');
+$.ajax({
+    method: 'GET',
+    url: 'https://unsplash.it/list',
+    success: function(result) {
+        // this is where the picture coming back from the AJAX request can be manipulated
+        const randomImg = result[Math.floor(Math.random() * result.length)];
+        const imgId = randomImg.id;
+        let currentImg = '<img src="https://unsplash.it/1200/800?image=' + imgId + '">';
+        contentsParent.prepend(currentImg);
+        contentsParent.prepend('<p class="beautText">Imagine how productive you can be without watching YouTube videos!</p>');
+    },
+    error: function(err) {
+        // if any errors occur during the process you can check out the error by logging the 'err' argument
+    }
+});
